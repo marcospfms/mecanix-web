823b79157d41169ccb6d3d805806a073da796e1a
Marcos Mamede <marcospaulo.20@gmail.com>
2026-04-03 12:32:46 -0400

    Fix: corrigir lint do runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,8 +20,8 @@ export default defineNuxtConfig({
       apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:8000/api',
       appUrl: process.env.NUXT_PUBLIC_APP_URL || 'http://localhost:3000',
       googleClientId:
-        process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID ||
-        process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID_WEB ||
+        process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID
+        || process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID_WEB
         ''
     }
   },
